24ce98662a10b7a11aa39f1514d27a8c4444ad57
Jordan David Evans <jordanevansprofessional@gmail.com>
2024-05-11 11:23:39 +1000

    1

---
+++ scripts/NavRoom.gd
@@ -0,0 +1,23 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_nav_button_pressed():
+	print("nav")
+	pass # Replace with function body.
+
+func _on_rudder_button_pressed():
+	print("rudder")
+	pass # Replace with function body.
+
+func _on_engine_button_pressed():
+	print("engine")
+	pass # Replace with function body.
